48a8d3ced03338055707ebfccbd1ae2d43ebd49c
rosenkranzAusbildung <rosenkranz@ausbildung-bbw.de>
2026-01-15 10:09:14 +0100

    removed the comments

--- config/10-native.sh
+++ config/10-native.sh
@@ -1,60 +1,60 @@
-alacritty # A cross-platform, GPU-accelerated terminal emulator
-alsa-firmware # Firmware binaries for loader programs in alsa-tools and hotplug firmware loader
-alsa-plugins # Additional ALSA plugins
-alsa-utils # Advanced Linux Sound Architecture - Utilities
-aspell # A spell checker designed to eventually replace Ispell
-cantarell-fonts # Humanist sans serif font
-cool-retro-term # A good looking terminal emulator which mimics the old cathode display
-cowsay # Configurable talking cow (and a bit more)
-cups # OpenPrinting CUPS - daemon package
-cups-browsed # a helper daemon to browse the network for remote CUPS queues and IPP network printers
-cups-filters # OpenPrinting CUPS Filters
-cups-pdf # PDF printer for cups
-fastfetch # A feature-rich and performance oriented neofetch like system information tool
-fbset # Framebuffer setup utility
-ffmpegthumbnailer # Lightweight video thumbnailer that can be used by file managers
-firefox # Fast, Private & Safe Web Browser
-fish # Smart and user friendly shell intended mostly for interactive use
-git # the fast distributed version control system
-gutenprint # Top quality printer drivers for POSIX systems
-htop # Interactive process viewer
-impala # TUI for managing wifi
-lxsession # Lightweight X11 session manager
-ly # TUI display manager
-mplayer # Media player for Linux
-neovim # Fork of Vim aiming to improve user experience, plugins, and GUIs
-netctl # Profile based systemd network management
-networkmanager # Network connection manager and user applications
-networkmanager-openconnect # NetworkManager VPN plugin for OpenConnect (with GUI)
-networkmanager-openvpn # NetworkManager VPN plugin for OpenVPN (with GUI)
-niri # A scrollable-tiling Wayland compositor
-noto-fonts # Google Noto TTF fonts
-noto-fonts-cjk # Google Noto CJK fonts
-noto-fonts-emoji # Google Noto Color Emoji font
-noto-fonts-extra # Google Noto TTF fonts - additional variants
-nwg-look # GTK settings editor adapted to work on wlroots-based compositors
-pipewire-alsa # Low-latency audio/video router and processor - ALSA configuration
-pipewire-jack # Low-latency audio/video router and processor - JACK replacement
-pipewire-pulse # Low-latency audio/video router and processor - PulseAudio replacement
-plymouth # Graphical boot splash screen
-rofi # A window switcher, application launcher and dmenu replacement
-thunar # Modern, fast and easy-to-use file manager for Xfce
-timeshift # A system restore utility for Linux
-ttf-bitstream-vera # Bitstream Vera fonts.
-ttf-dejavu # Font family based on the Bitstream Vera Fonts with a wider range of characters
-ttf-liberation # Font family which aims at metric compatibility with Arial, Times New Roman, and Courier New
-ttf-opensans # Sans-serif typeface commissioned by Google
-wev # A tool for debugging wayland events on a Wayland window, analagous to the X11 tool xev
-wireplumber # Session / policy manager implementation for PipeWire
-wl-clipboard # Command-line copy/paste utilities for Wayland
-xdg-desktop-portal-gtk # A backend implementation for xdg-desktop-portal using GTK
-xdg-user-dirs # Manage user directories like ~/Desktop and ~/Music
-xdg-utils # Command line tools that assist applications with a variety of desktop integration tasks
-xorg-server # Xorg X server
-xorg-xdpyinfo # Display information utility for X
-xorg-xinit # X.Org initialisation program
-xorg-xinput # Small commandline tool to configure devices
-xorg-xkill # Kill a client by its X resource
-xorg-xrandr # Primitive command line interface to RandR extension
-xterm # X Terminal Emulator
-xwayland-satellite # Xwayland outside your Wayland
+alacritty
+alsa-firmware
+alsa-plugins
+alsa-utils
+aspell
+cantarell-fonts
+cool-retro-term
+cowsay
+cups
+cups-browsed
+cups-filters
+cups-pdf
+fastfetch
+fbset
+ffmpegthumbnailer
+firefox
+fish
+git
+gutenprint
+htop
+impala
+lxsession
+ly
+mplayer
+neovim
+netctl
+networkmanager
+networkmanager-openconnect
+networkmanager-openvpn
+niri
+noto-fonts
+noto-fonts-cjk
+noto-fonts-emoji
+noto-fonts-extra
+nwg-look
+pipewire-alsa
+pipewire-jack
+pipewire-pulse
+plymouth
+rofi
+thunar
+timeshift
+ttf-bitstream-vera
+ttf-dejavu
+ttf-liberation
+ttf-opensans
+wev
+wireplumber
+wl-clipboard
+xdg-desktop-portal-gtk
+xdg-user-dirs
+xdg-utils
+xorg-server
+xorg-xdpyinfo
+xorg-xinit
+xorg-xinput
+xorg-xkill
+xorg-xrandr
+xterm
+xwayland-satellite
--- config/20-foreign.sh
+++ config/20-foreign.sh
@@ -1,6 +1,6 @@
-awww-git # Efficient animated wallpaper daemon for Wayland, controlled at runtime
-neo-matrix # Simulates the digital rain from "The Matrix"
-pipes.sh # Animated pipes terminal screensaver
-plymouth-theme-mikuboot-git # A miku boot animation because she is living in my computer.
-plymouth-theme-psx # PSX boot simulation
-tty-clock # Digital clock in ncurses
+awww-git
+neo-matrix
+pipes.sh
+plymouth-theme-mikuboot-git
+plymouth-theme-psx
+tty-clock
